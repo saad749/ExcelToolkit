9c4795a7599d6c15e5340fcf225f010e86c5b509
Saad Farooq <saad749@yahoo.com>
2018-01-12 01:47:07 +0300

    v1.0
    Rename Excel Workbooks and Worksheets

---
+++ ExcelToolkit/ExcelToolkit.FormUI/ExcelToolkitForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace ExcelToolkit.FormUI
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.btnOpenFolderDialog = new System.Windows.Forms.Button();
+            this.txtFolderPath = new System.Windows.Forms.TextBox();
+            this.pnlSub = new System.Windows.Forms.Panel();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.tvFiles = new System.Windows.Forms.TreeView();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.msMain = new System.Windows.Forms.MenuStrip();
+            this.tsmiFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiGettingStarted = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlSub.SuspendLayout();
+            this.pnlMain.SuspendLayout();
+            this.msMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpenFolderDialog
+            // 
+            this.btnOpenFolderDialog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFolderDialog.Location = new System.Drawing.Point(672, 26);
+            this.btnOpenFolderDialog.Name = "btnOpenFolderDialog";
+            this.btnOpenFolderDialog.Size = new System.Drawing.Size(104, 23);
+            this.btnOpenFolderDialog.TabIndex = 1;
+            this.btnOpenFolderDialog.Text = "Select Folder";
+            this.btnOpenFolderDialog.UseVisualStyleBackColor = true;
+            this.btnOpenFolderDialog.Click += new System.EventHandler(this.btnOpenFolderDialog_Click);
+            // 
+            // txtFolderPath
+            // 
+            this.txtFolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolderPath.Location = new System.Drawing.Point(6, 28);
+            this.txtFolderPath.Name = "txtFolderPath";
+            this.txtFolderPath.Size = new System.Drawing.Size(660, 20);
+            this.txtFolderPath.TabIndex = 2;
+            // 
+            // pnlSub
+            // 
+            this.pnlSub.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlSub.Controls.Add(this.btnRename);
+            this.pnlSub.Controls.Add(this.tvFiles);
+            this.pnlSub.Location = new System.Drawing.Point(3, 54);
+            this.pnlSub.Name = "pnlSub";
+            this.pnlSub.Size = new System.Drawing.Size(778, 704);
+            this.pnlSub.TabIndex = 4;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRename.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnRename.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRename.ForeColor = System.Drawing.Color.White;
+            this.btnRename.Location = new System.Drawing.Point(3, 649);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(770, 52);
+            this.btnRename.TabIndex = 2;
+            this.btnRename.Text = "RENAME";
+            this.btnRename.UseVisualStyleBackColor = false;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // tvFiles
+            // 
+            this.tvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tvFiles.LabelEdit = true;
+            this.tvFiles.Location = new System.Drawing.Point(3, 3);
+            this.tvFiles.Name = "tvFiles";
+            this.tvFiles.ShowNodeToolTips = true;
+            this.tvFiles.Size = new System.Drawing.Size(770, 640);
+            this.tvFiles.TabIndex = 0;
+            this.tvFiles.AfterLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.tvFiles_AfterLabelEdit);
+            this.tvFiles.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvFiles_NodeMouseClick);
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.btnOpenFolderDialog);
+            this.pnlMain.Controls.Add(this.txtFolderPath);
+            this.pnlMain.Controls.Add(this.pnlSub);
+            this.pnlMain.Controls.Add(this.msMain);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(784, 761);
+            this.pnlMain.TabIndex = 5;
+            // 
+            // msMain
+            // 
+            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiFile,
+            this.helpToolStripMenuItem});
+            this.msMain.Location = new System.Drawing.Point(0, 0);
+            this.msMain.Name = "msMain";
+            this.msMain.Size = new System.Drawing.Size(784, 24);
+            this.msMain.TabIndex = 5;
+            this.msMain.Text = "Menu";
+            // 
+            // tsmiFile
+            // 
+            this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiExit});
+            this.tsmiFile.Name = "tsmiFile";
+            this.tsmiFile.Size = new System.Drawing.Size(37, 20);
+            this.tsmiFile.Text = "&File";
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(152, 22);
+            this.tsmiExit.Text = "&Exit";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiGettingStarted,
+            this.tsmiAbout});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // tsmiGettingStarted
+            // 
+            this.tsmiGettingStarted.Name = "tsmiGettingStarted";
+            this.tsmiGettingStarted.Size = new System.Drawing.Size(157, 22);
+            this.tsmiGettingStarted.Text = "&Gettting Started";
+            this.tsmiGettingStarted.Click += new System.EventHandler(this.tsmiGettingStarted_Click);
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.Size = new System.Drawing.Size(157, 22);
+            this.tsmiAbout.Text = "&About";
+            this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 761);
+            this.Controls.Add(this.pnlMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.msMain;
+            this.Name = "formMain";
+            this.Text = "Excel Toolkit";
+            this.pnlSub.ResumeLayout(false);
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.msMain.ResumeLayout(false);
+            this.msMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOpenFolderDialog;
+        private System.Windows.Forms.TextBox txtFolderPath;
+        private System.Windows.Forms.Panel pnlSub;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.TreeView tvFiles;
+        private System.Windows.Forms.MenuStrip msMain;
+        private System.Windows.Forms.ToolStripMenuItem tsmiFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiGettingStarted;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+    }
+}
+
---
+++ ExcelToolkit/ExcelToolkit.FormUI/ExcelToolkitForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace ExcelToolkit.FormUI
+{
+    public partial class formMain : Form
+    {
+       
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpenFolderDialog_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog cofdMain = new CommonOpenFileDialog();
+            cofdMain.IsFolderPicker = true;
+            if (cofdMain.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                PopulateTreeView(cofdMain.FileName);
+            }
+        }
+
+        private void PopulateTreeView(string path)
+        {
+            DirectoryInfo dInfo = new DirectoryInfo(path);
+            FileInfo[] files = dInfo.GetFiles("*.xlsx");
+            txtFolderPath.Text = path;
+            tvFiles.Nodes.Clear();
+            foreach (var file in files)
+            {
+                TreeNode fileNode = new TreeNode
+                {
+                    Text = file.Name,
+                    Tag = file.Name //Will keep the orginal name of the file/sheet
+                };
+                //tvFiles.Nodes
+                var sheets = GetAllWorksheets(file.FullName);
+                foreach (Sheet sheet in sheets)
+                {
+                    TreeNode sheetNode = new TreeNode
+                    {
+                        Text = sheet.Name,
+                        ToolTipText = sheet.Name.Value.Length.ToString("##"),
+                        Tag = sheet.Name, //Will keep the orginal name of the file/sheet
+                        BackColor = System.Drawing.Color.LimeGreen
+                    };
+                    if (sheetNode.Text.Length > 31)
+                        sheetNode.BackColor = System.Drawing.Color.Pink;
+                    fileNode.Nodes.Add(sheetNode);
+                }
+                tvFiles.Nodes.Add(fileNode);
+            }
+            tvFiles.ExpandAll();
+
+        }
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            pnlMain.Enabled = false;
+            pnlMain.Visible = false;
+
+            Label lblLoading = new Label()
+            {
+                AutoSize = true,
+                Text = "Please Wait ...",
+                ForeColor = System.Drawing.Color.Green,
+                Font = new System.Drawing.Font("Verdana", 24, FontStyle.Bold)
+            };
+            lblLoading.Top = (pnlMain.Height / 2) - (lblLoading.Height);
+            lblLoading.Left = (pnlMain.Width / 2) - (lblLoading.Width);
+            this.Controls.Add(lblLoading);
+            this.Refresh();
+
+            var path = txtFolderPath.Text + "\\";
+
+
+            foreach (TreeNode fileNode in tvFiles.Nodes)
+            {
+                if(fileNode.Text != fileNode.Tag.ToString())
+                {
+                    File.Move(path + fileNode.Tag.ToString(), path + fileNode.Text);
+                }
+
+                bool isAnySheetRenamed = false;
+                foreach (TreeNode sheetNode in fileNode.Nodes)
+                {
+                    if (sheetNode.Text != sheetNode.Tag.ToString())
+                    {
+                        isAnySheetRenamed = true;
+                    }
+                }
+
+                if (isAnySheetRenamed)
+                {
+                    using (SpreadsheetDocument document = SpreadsheetDocument.Open(path + fileNode.Text, true))
+                    {
+                        WorkbookPart wbPart = document.WorkbookPart;
+                        foreach (Sheet sheet in wbPart.Workbook.Sheets)
+                        {
+                            foreach (TreeNode sheetNode in fileNode.Nodes)
+                            {
+                                if (sheetNode.Tag.ToString() == sheet.Name && sheetNode.Text != sheetNode.Tag.ToString())
+                                {
+                                    sheet.Name = sheetNode.Text;
+                                }
+                            }
+                        }
+                        document.Save();
+                    }
+                }
+            }
+
+            PopulateTreeView(txtFolderPath.Text);
+            MessageBox.Show("The items have been renamed successfully", "Rename Successfull");
+            pnlMain.Enabled = true;
+            pnlMain.Visible = true;
+            this.Controls.Remove(lblLoading);
+            this.Refresh();
+        }
+
+        public string[] GetSheetNames(FileInfo file)
+        {
+            List<string> sheets = new List<string>();
+            var items = GetAllWorksheets(file.FullName);
+            foreach (Sheet sheet in items)
+            {
+                sheets.Add(sheet.Name);
+            }
+
+            return sheets.ToArray();
+        }
+
+        // Retrieve a List of all the sheets in a workbook.
+        // The Sheets class contains a collection of 
+        // OpenXmlElement objects, each representing one of 
+        // the sheets.
+        public static Sheets GetAllWorksheets(string fileName)
+        {
+            Sheets theSheets = null;
+
+            using (SpreadsheetDocument document =
+                SpreadsheetDocument.Open(fileName, false))
+            {
+                WorkbookPart wbPart = document.WorkbookPart;
+                theSheets = wbPart.Workbook.Sheets;
+            }
+            return theSheets;
+        }
+
+
+        private void tvFiles_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            tvFiles.SelectedNode = null;
+            e.Node.BeginEdit();
+        }
+
+        private void tvFiles_AfterLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+            if (e.Label == null)
+                return;
+            e.Node.ToolTipText = e.Label.Length.ToString("##");
+            if (e.Label.Length > 31 && e.Node.Parent != null)
+            {
+                e.Node.ToolTipText = "Reduce Sheet Name to less than 31 characters: " + e.Label.Length.ToString("##");
+                e.Node.BackColor = System.Drawing.Color.Pink;
+            }
+            else if (e.Node.Parent != null)
+                e.Node.BackColor = System.Drawing.Color.LimeGreen;
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("v1.0.0" + Environment.NewLine +
+                "Developed by Saad Farooq" + Environment.NewLine +
+                "http://saadfarooq.net",
+                "About Excel Toolkit");
+        }
+
+        private void tsmiGettingStarted_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a simple tool to rename excel workbooks and worksheets without opening them. " +
+                Environment.NewLine + Environment.NewLine +
+                "To get started: " + Environment.NewLine +
+                "Select the folder with the excel files" + Environment.NewLine +
+                "The files with the sheets will appear in a tree strcture" + Environment.NewLine +
+                "Click any name in the tree to rename it" + Environment.NewLine +
+                "Once all names have been updated, Click the Rename button" + Environment.NewLine + Environment.NewLine +
+                "Renaming files are much faster than renaming sheets. Renaming sheets of huge excel files may take a few seconds" +
+                Environment.NewLine + Environment.NewLine +
+                "http://saadfarooq.net",
+                "Getting Started");
+        }
+    }
+}
